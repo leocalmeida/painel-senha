ada00631d59d4df02620a090f6fa18faf7172e2b
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-17 09:36:10 -0300

    adicionando o deploy da pagina nas paginas do github

---
+++ build/precache-manifest.08d5eabde4f05edaf71cafaf0ef259fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e18a573db79e5f93b30aa0142767735",
+    "url": "/painel-senha/index.html"
+  },
+  {
+    "revision": "a014e2c943f423fd890b",
+    "url": "/painel-senha/static/css/main.7cede82f.chunk.css"
+  },
+  {
+    "revision": "c72a5022f4a207676119",
+    "url": "/painel-senha/static/js/2.a2e56d73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/painel-senha/static/js/2.a2e56d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a014e2c943f423fd890b",
+    "url": "/painel-senha/static/js/main.13f07c43.chunk.js"
+  },
+  {
+    "revision": "fe1eb438e0327901451a",
+    "url": "/painel-senha/static/js/runtime-main.5bf51a5e.js"
+  }
+]);
